a72ca6f4bbeaa3e9e42c0ef354ec22de33570476
Your Name <youremail@example.com>
2025-04-17 19:26:05 +0300

    Add Homework 2 notebook, and clean up old files

--- bubble.py
+++
@@ -1,8 +0,0 @@
-arr=[4,4,3,100,1,8]
-for i in range(len(arr)):
-    for j in range(i+1,len(arr)):
-        if arr[i] > arr[j]:
-           temp=arr[j]
-           arr[i]+arr[j]
-           arr[j]=temp
-print(arr)        
--- linear.py
+++
@@ -1,9 +0,0 @@
-flag=0
-array=[2,3,4,5,10,59,89,78,900]
-target=int(input("enter item"))
-for counter in array:
-    if flag==1:
-        print("item is found")
-        break
-if flag==0:
-    print("item is not found")    
